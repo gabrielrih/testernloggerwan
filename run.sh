e58350909628db3801239d340b05aa11dce140e8
Gabriel Richter <gabriel.richter@ambevtech.com.br>
2022-05-31 12:00:57 -0300

    New feature - add more than one log file

--- run.sh
+++ run.sh
@@ -5,15 +5,19 @@
 # By gabrielrih <gabrielrih@gmail.com>
 #
 
-INTERVAL_TO_TEST_CONNECTION_IN_SECONDS=10
+INTERVAL_TO_TEST_CONNECTION_IN_SECONDS=2
 LOG_FOLDER="/var/log/testerNlogger"
-LOG_FILENAME="connection.log"
+LOG_FILENAME="connection.log" # in runtime this name is changed
 LOG_WHEN_CHANGE_FILENAME="changeHistory.log"
+CLEAR_ALL_LOG_FILES_EVERY_START=false
 LOG_FULL_PATH=$LOG_FOLDER/$LOG_FILENAME
 LOG_ON_CHANGE_FULL_PATH=$LOG_FOLDER/$LOG_WHEN_CHANGE_FILENAME
-CLEAR_LOG_FILES_EVERY_START=false # for testing usage
 
-# RUN AS ADMIN
+# Is it root?
+if [ $(whoami) != root ]; then
+    echo -e "Error! Run it as root!"
+    exit 1
+fi
 
 testConnection() {
     ping 8.8.8.8 -c 1 > .result.tmp
@@ -23,14 +27,13 @@ testConnection() {
 
 createLogContent() {
     if [ $isDown -eq 1 ]; then status='down'; else status='up'; fi
-    logContent="$(date +%Y-%m-%d)|$(date +%H:%M:%S)|$status|"
+    logContent="$currentDate|$currentTime|$status|"
 }
 
 logWhenStatusChanged() {
     if [ ! $isDownLast ]; then isDownLast=2; fi # first execution. Force value two to print changed log
     # log file. Format: date|time|status|
     if [ ! -f $LOG_ON_CHANGE_FULL_PATH ]; then echo "date|time|status|" >> $LOG_ON_CHANGE_FULL_PATH; fi # print header if log file doesn't exists
-    createLogContent
     if [ $isDown -ne $isDownLast ]; then echo $logContent >> $LOG_ON_CHANGE_FULL_PATH; fi
     # set current as last
     isDownLast=$isDown
@@ -38,26 +41,28 @@ logWhenStatusChanged() {
 
 logEverything() {
     # standard stdout
+    currentDatetime=$currentDate"T"$currentTime
     if [ $isDown -eq 1 ]; then echo "[-] Connection is DOWN! ($currentDatetime)"; else echo "[+] Connection is UP! ($currentDatetime)"; fi
     # log file. Format: date|time|status|
-    if [ ! -f $LOG_FULL_PATH ]; then echo "date|time|status|" >> $LOG_FULL_PATH; fi # print header if log file doesn't exists
-    createLogContent
-    echo $logContent >> $LOG_FULL_PATH
+    logFullPathWithDate=$(echo $LOG_FULL_PATH | sed "s/\.log/-$currentDate.log/g")
+    if [ ! -f $logFullPathWithDate ]; then echo "date|time|status|" >> $logFullPathWithDate; fi # print header if log file doesn't exists
+    echo $logContent >> $logFullPathWithDate
 }
 
 cleaningLogFiles() {
-    echo "[!] Cleaning old log files!"
-    rm $LOG_FULL_PATH
-    rm $LOG_ON_CHANGE_FULL_PATH
+    echo "[!] Deleting all log files!"
+    rm -r $LOG_FOLDER
 }
 
 echo "Testing WAN connection every $INTERVAL_TO_TEST_CONNECTION_IN_SECONDS seconds..."
-if [ $CLEAR_LOG_FILES_EVERY_START == true ]; then cleaningLogFiles; fi
+if [ $CLEAR_ALL_LOG_FILES_EVERY_START == true ]; then cleaningLogFiles; fi
 while true
 do
-    currentDatetime=$(date +%Y-%m-%dT%H:%M:%S)
+    currentDate=$(date +%Y-%m-%d)
+    currentTime=$(date +%H:%M:%S)
     testConnection
     if [ ! -d $LOG_FOLDER ]; then mkdir $LOG_FOLDER; fi
+    createLogContent
     logWhenStatusChanged
     logEverything
     sleep $INTERVAL_TO_TEST_CONNECTION_IN_SECONDS
